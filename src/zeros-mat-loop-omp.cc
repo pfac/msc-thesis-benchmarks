fc8ed318c754fb4f72ad43774229b8614f35f53f
Pedro Costa <dev@iampfac.com>
2013-05-11 21:47:29 +0100

    Added new case for the zeros benchmark.
    * zeros-mat-loop-omp: Allocate a matrix with Armadillo and set all
      elements to zero individually, but using OpenMP.

---
+++ src/zeros-mat-loop-omp.cc
@@ -0,0 +1,36 @@
+// c++ std headers
+#include <iostream>
+	using std::clog;
+	using std::cout;
+
+// other headers
+#include <cuk/stopwatch>
+	using cuk::stopwatch;
+
+#include <armadillo>
+	using arma::Mat;
+	using arma::zeros;
+
+#define endl '\n'
+
+
+
+int main () {
+	const unsigned n = DIMENSION;
+
+	stopwatch sw;
+	sw.start();
+	Mat<double> matrix(n,n);
+	#pragma omp parallel for
+	for (unsigned i = 0; i < n; ++i)
+	{
+		#pragma omp parallel for
+		for (unsigned j = 0; j < n; ++j)
+			matrix(i,j) = 0;
+	}
+	sw.stop();
+
+	cout << sw.ns() << endl;
+
+	return 0;
+}
